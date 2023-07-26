da5b44e99c429a3f8134c8c87a591ce692779353
ageka02 <aginanjar377@gmail.com>
2023-07-26 14:43:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -29,10 +29,21 @@
 // })
 
 // index.js
+const bodyParser = require('body-parser');
 const express = require('express')
+
+ const eventRoutes = require('./routes/events');
 
 const app = express()
 const PORT = 4000
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
