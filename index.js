89d4bc2ecb6134caf67c3877baf74e23469df7fe
ageka02 <aginanjar377@gmail.com>
2023-07-26 14:40:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
-const bodyParser = require('body-parser');
-const express = require('express');
+// const bodyParser = require('body-parser');
+// const express = require('express');
 
-const eventRoutes = require('./routes/events');
+// const eventRoutes = require('./routes/events');
 
-const app = express();
-const port = 8080
+// const app = express();
+// const port = 8080
 
-app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.use(bodyParser.json());
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
-app.use('/events', eventRoutes);
+// app.use('/events', eventRoutes);
 
-app.use((error, req, res, next) => {
-  const status = error.status || 500;
-  const message = error.message || 'Something went wrong.';
-  res.status(status).json({ message: message });
-});
+// app.use((error, req, res, next) => {
+//   const status = error.status || 500;
+//   const message = error.message || 'Something went wrong.';
+//   res.status(status).json({ message: message });
+// });
 
-app.listen(port, () => { console.log(`API listening on PORT ${port}`) } );
+// app.listen(port, () => { console.log(`API listening on PORT ${port}`) } );
+
+// app.get('/', (req, res) => {
+//   res.send('Hey this is my API running 🥳')
+// })
+
+// index.js
+const express = require('express')
+
+const app = express()
+const PORT = 4000
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
+})
 
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
 })
+
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
+
+// Export the Express API
+module.exports = app
